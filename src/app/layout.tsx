a0ac669b11c10c03f17348ce69e4cd8338031aa5
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-06-22 17:36:55 +0300

    hhh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,29 @@
 
 import "./globals.css";
-import Header from "../components/Header";
-export const metadata={
-  title: "The-Echo",
+import Header from "@/src/components/Header";
+import Footer from "@/src/components/Footer";
+
+export const metadata = {
+  title: "The Echo",
   description: "Echoing Reality of Thousands...",
 };
 
 export default function RootLayout({
   children,
-}:{
+}: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>
-        <Header/>
-        {children}
+      <body className="min-h-screen flex flex-col">
+
+        <Header />
+
+        <main className="flex-1">
+          {children}
+        </main>
+        <Footer/>
+
       </body>
     </html>
   );
