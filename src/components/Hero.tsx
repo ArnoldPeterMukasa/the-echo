e495b0bf008597e61048445063c4b7c6f6bf2092
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-06-22 16:55:51 +0300

    hhh

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+    return(
+        <div>
+            <h1 style={{fontSize: "42px", fontWeight: "bold"}}>
+                The Echo
+            </h1>
+            <p style={{color: "#555", fontSize: "18px"}}>
+                Echoing Reality of Thousands...
+            </p>
+        </div>
+    )
+}
