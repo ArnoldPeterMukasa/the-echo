c27bb4697d125b9bf12e1bcf21747557ece54792
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-06-22 16:57:57 +0300

    hhh

---
+++ src/types/article.ts
@@ -0,0 +1,11 @@
+export type Article={
+    id: string;
+    title: string;
+    slug: string;
+    excerpt: string;
+    content: string;
+    coverImage?: string;
+    author: string;
+    category: string;
+    createdAt: string;
+};
