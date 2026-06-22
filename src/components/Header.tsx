24b918d39a55f04d33f86a83029c29a3b486c8d1
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-06-22 17:35:54 +0300

    hhh

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,35 @@
 import Link from "next/link";
-export default function Header(){
-    return(
-        <header className="border-b">
-            <div className="max-w-5xl mx-auto px-6 py-4 flex justify-between item-center">
-                {/*logo*/}
-                <Link href="/" className="text-2xl font-bold">
-                    The Echo                
-                </Link>
-                {/*Navigation*/}
-                <nav className="flex gap-6 text-sm">
-                    <Link href="/">Home</Link>
-                    <Link href="/articles">Articles</Link>
-                    <Link href="/about">About</Link>
-                    <Link href="/contact">Contact</Link>
-                </nav>
-            </div>
-        </header>
-    );
+
+export default function Header() {
+  return (
+    <header className="border-b">
+      <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
+
+        {/* Logo */}
+        <Link href="/" className="text-2xl font-bold">
+          The Echo
+        </Link>
+
+        {/* Nav */}
+        <nav className="flex gap-6 text-sm text-gray-700">
+          <Link href="/" className="hover:text-black">
+            Home
+          </Link>
+
+          <Link href="/articles" className="hover:text-black">
+            Articles
+          </Link>
+
+          <Link href="/about" className="hover:text-black">
+            About
+          </Link>
+
+          <Link href="/contact" className="hover:text-black">
+            Contact
+          </Link>
+        </nav>
+
+      </div>
+    </header>
+  );
 }
