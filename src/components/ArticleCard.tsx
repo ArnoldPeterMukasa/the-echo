28adb999757961ec0300aa84249d79206b4689e5
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-06-22 16:56:28 +0300

    hhh

---
+++ src/components/ArticleCard.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+type Props={
+    title: string;
+    excerpt: string;
+    slug?: string;
+};
+export default function ArticleCard({
+    title, 
+    excerpt,
+    slug,
+
+}: Props) {
+    return (
+        <Link href={'/articles/${slug}'}>
+            <div className="border rounded-lg p-5 hover:shadow-md transition">
+                <h2 className="text-2xl font-bold">
+                    {title}
+                </h2>
+
+                <p className="text-gray-600 mt-2">
+                    {excerpt}
+                </p>
+                </div>
+        </Link>
+    );
+}
