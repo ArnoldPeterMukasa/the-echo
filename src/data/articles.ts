ff4dafce42bb96a7499786921b547240165bd84a
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-06-22 16:57:23 +0300

    hhh

---
+++ src/data/articles.ts
@@ -0,0 +1,23 @@
+import {Article} from "@/src/types/article";
+export const articles: Article[] = [
+    {
+        id: "1",
+        title: "Weclome to The Echo",
+        slug: "welcome-to-the-echo",
+        excerpt: "The Voice of the Core",
+        content: "First Article of The Echo...",
+        author: "admin",
+        category: "Announcement",
+        createdAt: "22-06-2026",
+    },
+    {
+    id: "2",
+    title: "Beginning Year Three",
+    slug: "beginning-year-three",
+    excerpt: "Reflections and expectations entering year three.",
+    content: "As we enter year three...",
+    author: "Editorial Team",
+    category: "Campus Life",
+    createdAt: "22-06-2026",
+  },
+];
