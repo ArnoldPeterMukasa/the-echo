ba9313dbd431f2e75c39b3a5fd78b57e4ce2b3d1
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-06-25 14:24:39 +0300

    ddd

--- src/types/article.ts
+++ src/types/article.ts
@@ -1,12 +1,30 @@
+export type ArticleStatus =
+  | "draft"
+  | "pending"
+  | "published"
+  | "rejected";
+
+
 export type Article = {
   id: string;
+
   title: string;
+
   slug: string;
+
   excerpt: string;
+
   content: string;
+
   author: string;
+
   category: string;
+
   createdAt: string;
-  trending?: boolean;
+
   coverImage: string;
+
+  trending?: boolean;
+
+  status: ArticleStatus;
 };
