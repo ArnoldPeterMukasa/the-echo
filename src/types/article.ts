e0df51a80d118801a4f7ef309af7d37cd2702dee
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-06-25 11:52:33 +0300

    rrrr

--- src/types/article.ts
+++ src/types/article.ts
@@ -1,14 +1,12 @@
-export type Article={
-    id: string;
-    title: string;
-    slug: string;
-    excerpt: string;
-    content: string;
-    coverImage?: string;
-    author: string;
-    category: string;
-    createdAt: string;
-    featured?: boolean;
-    trending?: boolean;
-    coverImage?: string;
+export type Article = {
+  id: string;
+  title: string;
+  slug: string;
+  excerpt: string;
+  content: string;
+  author: string;
+  category: string;
+  createdAt: string;
+  trending?: boolean;
+  coverImage: string;
 };
