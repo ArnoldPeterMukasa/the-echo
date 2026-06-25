8d35d8958f975af928406e0ad4f8c3c29d578e3c
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-06-25 14:26:05 +0300

    ddd

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,21 +1,15 @@
-import ArticleCard from "@/src/components/ArticleCard";
-import { articles } from "@/src/data/articles";
-export default function ArticlePage(){
-    return (
-        <main className="p-10">
-            <h1 className="text-4xl font-bold mb-8">
-                Articles
-            </h1>
-            <div className="space-y-4">
-                {articles.map((article)=>(
-                    <ArticleCard
-                    key={article.id}
-                    title={article.title}
-                    excerpt={article.excerpt}
-                    slug={article.slug}
-                    />
-                ))}
-            </div>
-        </main>
-    );
+import SearchArticles from "@/src/components/articles/SearchArticles";
+
+export default function ArticlesPage() {
+  return (
+    <main className="max-w-6xl mx-auto px-6 py-12">
+
+      <h1 className="text-4xl font-bold mb-10">
+        All Articles
+      </h1>
+
+      <SearchArticles />
+
+    </main>
+  );
 }
