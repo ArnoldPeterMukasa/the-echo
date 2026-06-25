85bc0e35fad9dbd28e4aabd69ec134159c6ae925
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-06-25 14:25:04 +0300

    ddd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { articles } from "@/src/data/articles";
+import { getArticles } from "@/src/lib/getArticles";
 import FeaturedSection from "@/src/components/Home/FeaturedSection";
 import LatestSection from "@/src/components/Home/LatestSection";
 import TrendingSection from "@/src/components/Home/TrendingSection";
@@ -6,8 +6,14 @@ import CategorySection from  "@/src/components/Home/CategorySection";
 import NewsletterSEction from "@/src/components/Home/NewsletterSection";
 
 export default function Home() {
-  const featured = articles[0];
-  const rest = articles.slice(1);
+  const articles = getArticles();
+
+  const published = articles.filter(
+  (a) => a.status === "published"
+  );
+
+  const featured = published[0];
+  const rest = published.slice(1);
 
   return (
     <main className="max-w-6xl mx-auto px-6 py-10">
