b7fe51196a7661e97d5c4133c105f33b90d7b2df
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-06-29 17:38:52 +0300

    aaaaa

--- src/lib/uploadImage.ts
+++ src/lib/uploadImage.ts
@@ -1,13 +1,12 @@
-export async function uploadImage(file: File) {
+export async function uploadImage(file: File): Promise<string> {
   const formData = new FormData();
 
   formData.append("file", file);
-  formData.append("upload_preset", "echo_upload");
-
-  const cloudName = "dv1sjsyr5"; // 🔴 cloudinary name
+  formData.append("upload_preset", "unsigned_upload"); 
+  // 👆 you will create this in Cloudinary
 
   const res = await fetch(
-    `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+    "https://api.cloudinary.com/v1_1/dv1sjsyr5/image/upload",
     {
       method: "POST",
       body: formData,
