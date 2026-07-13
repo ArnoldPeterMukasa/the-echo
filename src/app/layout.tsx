4fadf1c7fe4a778982bcad2f8fa92fdae88b8517
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-07-13 17:39:42 +0300

    dddd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,50 @@
-
 import "./globals.css";
+
+import Providers from "@/src/components/Providers";
 import HeaderWrapper from "@/src/components/HeaderWrapper";
 import Footer from "@/src/components/Footer";
+import { AuthProvider } from "@/src/context/AuthContext";
+
 
 export const metadata = {
   title: "The Echo",
   description: "Echoing Reality of Thousands...",
 };
 
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
   return (
     <html lang="en">
+
       <body className="min-h-screen flex flex-col">
 
-        <HeaderWrapper />
+        <Providers>
 
-        <main className="flex-1">
-          {children}
-        </main>
-        
-        <Footer/>
+          <AuthProvider>
+
+            <HeaderWrapper />
+
+            <main className="flex-1">
+              {children}
+            </main>
+
+            <Footer />
+
+          </AuthProvider>
+
+        </Providers>
 
       </body>
+
     </html>
   );
 }
+
+
+
+
