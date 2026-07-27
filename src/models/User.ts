f2a536bb1f47476fd24ab7d18e41b01a1698497d
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-07-27 15:32:22 +0300

    sssdd

---
+++ src/models/User.ts
@@ -0,0 +1,69 @@
+import mongoose, { Schema, model, models } from "mongoose";
+
+
+const UserSchema = new Schema(
+
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+
+    role: {
+      type: String,
+      enum: [
+        "writer",
+        "admin"
+      ],
+      default: "writer",
+    },
+
+
+    image: {
+      type: String,
+      default: "",
+    },
+
+  },
+
+  {
+    timestamps: true,
+  }
+
+);
+
+
+
+const User =
+  models.User ||
+  model(
+    "User",
+    UserSchema
+  );
+
+
+export default User;
