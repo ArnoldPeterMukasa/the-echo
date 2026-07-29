0b54bad4a210bbd19c796189faea335143cd9a34
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-07-29 10:20:24 +0300

    dddd

---
+++ src/models/Article.ts
@@ -0,0 +1,59 @@
+import mongoose, { Schema, models } from "mongoose";
+
+const ArticleSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    excerpt: {
+      type: String,
+      default: "",
+    },
+
+    content: {
+      type: String,
+      required: true,
+    },
+
+    coverImage: {
+      type: String,
+      default: "",
+    },
+
+    category: {
+      type: String,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["draft", "pending", "published"],
+      default: "draft",
+    },
+
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    views: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default models.Article ||
+  mongoose.model("Article", ArticleSchema);
