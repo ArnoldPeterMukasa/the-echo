39e42ee5cd4ee81e9da9680c918d0df7dbe502bc
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-07-30 09:26:11 +0300

    dfdfdfdfdddddddddd

--- src/models/Article.ts
+++ src/models/Article.ts
@@ -1,78 +1,130 @@
 import mongoose, { Schema, models } from "mongoose";
 
+
 const ArticleSchema = new Schema(
+
   {
+
     title: {
+
       type: String,
+
       required: true,
+
     },
+
 
     slug: {
+
       type: String,
+
       required: true,
+
       unique: true,
+
     },
+
 
     excerpt: {
+
       type: String,
+
       default: "",
+
     },
+
 
     content: {
+
       type: String,
+
       required: true,
+
     },
+
 
     coverImage: {
+
       type: String,
+
       default: "",
+
     },
+
 
     category: {
+
       type: String,
+
       required: true,
+
+      default: "General",
+
     },
 
+
     status: {
+
       type: String,
+
       enum: [
         "draft",
         "pending",
         "published",
       ],
+
       default: "draft",
+
     },
+
 
     author: {
+
       type: Schema.Types.ObjectId,
+
       ref: "User",
+
       required: true,
+
     },
 
-    views: {
-      type: Number,
-      default: 0,
-    },
 
     featured: {
+
       type: Boolean,
+
       default: false,
+
     },
 
-    trending: {
-      type: Boolean,
-      default: false,
+
+    views: {
+
+      type: Number,
+
+      default: 0,
+
     },
+
 
   },
+
   {
-    timestamps: true,
+
+    timestamps:true,
+
   }
+
 );
 
 
-export default models.Article ||
+
+const Article =
+  models.Article ||
   mongoose.model(
     "Article",
     ArticleSchema
   );
+
+
+export default Article;
